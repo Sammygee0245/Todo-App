0052c6ddfdfd26724d335ec1b8f4e5431bf88087
Samuel Bediako <samuelbediako020.com>
2022-07-23 20:06:54 -0700

    Added a delete btn

--- index.js
+++ index.js
@@ -2,9 +2,13 @@ let input = document.querySelector("#input")
 let button = document.querySelector("#mybtn")
 let todo = document.querySelector("#container")
 let reset = document.querySelector("#clear")
+let date = document.querySelector("#date")
 
 //creating an array to store the values
 let todos = []
+
+
+function del(){}
 
 
 function rendering(){
@@ -14,13 +18,21 @@ function rendering(){
     todos.forEach(function(param){
         let element = document.createElement("h3")
         element.innerText = param
+        let deletebtn = document.createElement("button")
+        deletebtn.innerText = "Delete"
+        element.appendChild(deletebtn)
+        deletebtn.style =  "margin-left:17px; background-color:gold; padding:0; "
         todo.appendChild(element)
+
+        deletebtn.addEventListener("click" del)
     })
 }
 
 function addTodo(){
-    todos.push(input.value)
+    todos.push(date.value+"__"+input.value)
     rendering()
+    input.value = ""
+    date.value = ""
 }
 
 function clear(){
